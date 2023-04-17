1ebfd77aa90cf17440dba2e18ac1662e40ab659c
Qujuan <qujuan.miller@snhu.edu>
2023-04-17 08:35:08 -0400

    Setting up android status bar

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform, SafeAreaView } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 
+console.log();
 export default function App() {
   return (
     <>
-    <SafeAreaView style={{flex: 1}}>
-    <View style={{padding: 16, backgroundColor: "green"}}>
-      <Text>Search</Text>
-    </View>
-    <View style={{flex: 1, padding:16, backgroundColor: 'orange'}}>
-      <Text>
-        List
-      </Text>
-    </View>
-    </SafeAreaView>
-    <StatusBar style="auto" />
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "orange" }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
